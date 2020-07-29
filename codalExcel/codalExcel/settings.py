4cd0d1ecce7cb98e32460c1e5b0ef0a008a58b47
ghazale12kazemi <ghazale12kazemi@gmail.com>
2020-07-30 00:30:22 +0430

    correct

--- codalExcel/codalExcel/settings.py
+++ codalExcel/codalExcel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2$rbzd3l$@xq_x25zg27bf1xql1gaq)#$^98ki699t!^_+g^tj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

7adb66d812812a45b001c762d0d9b302764fca09
ghazale12kazemi <ghazale12kazemi@gmail.com>
2020-07-30 23:05:17 +0430

    correct

--- codalExcel/codalExcel/settings.py
+++ codalExcel/codalExcel/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '\lenovo\PycharmProjects\codalexcel\codalExcel\media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
